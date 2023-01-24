84bc2134f7ca379adb636ce2ba15fddec6c01379
Matthias Mosthof <matthias.mosthof@gmail.com>
2023-01-24 02:33:44 +0100

    adds logic to fetch artist specific data

--- src/app/test/test.component.ts
+++ src/app/test/test.component.ts
@@ -9,27 +9,41 @@ import { environment } from '../environments/environment';
 export class TestComponent {
   songs: any;
   country: string = 'spain';
+  artists: any;
 
   onCountrySelection(event: any) {
     this.country = event.target.value;
-    this.fetchData();
+    this.getTopArtist();
   }
 
-  async fetchData() {
+  async getTopArtist() {
     const apiKey = environment.apiKey;
-    const artist = 'Kim Petras';
-    const track = 'brrr';
     const url = `https://ws.audioscrobbler.com/2.0/?method=geo.gettopartists&country=${this.country}&api_key=${apiKey}&page=1&limit=10&format=json`;
     try {
       const response = await fetch(url);
       const data = await response.json();
       this.songs = data.topartists.artist;
+      this.artists = data.topartists.artist.map((artist: any) => {
+        return artist.name;
+      });
       console.log(this.songs);
+      console.log(this.artists);
     } catch (error) {
       console.error(error);
     }
   }
+  async getArtistInfo() {
+    const apiKey = environment.apiKey;
+    console.log(this.artists);
+    for (let artist of this.artists) {
+      const url = `https://ws.audioscrobbler.com/2.0/?method=artist.getinfo&artist=${artist}&api_key=${apiKey}&format=json`;
+      const response = await fetch(url);
+      const data = await response.json();
+      console.log(data.artist);
+    }
+  }
   async ngOnInit() {
-    this.fetchData();
+    await this.getTopArtist();
+    this.getArtistInfo();
   }
 }
