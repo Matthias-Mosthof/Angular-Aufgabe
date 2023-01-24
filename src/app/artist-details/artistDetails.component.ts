1d87efc863c9eef668d199aa63e764dc70c3b175
Matthias Mosthof <matthias.mosthof@gmail.com>
2023-01-24 16:38:28 +0100

    adds dynamic routing for artist details view

--- src/app/artist-details/artistDetails.component.ts
+++ src/app/artist-details/artistDetails.component.ts
@@ -1,8 +1,28 @@
 import { Component } from '@angular/core';
+import { environment } from '../environments/environment';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-artist-details',
   templateUrl: './artistDetails.component.html',
   styleUrls: ['./artistDetails.component.css'],
 })
-export class ArtistDetailsComponent {}
+export class ArtistDetailsComponent {
+  mbid: any;
+
+  constructor(private route: ActivatedRoute) {}
+  async getArtistInfo() {
+    const apiKey = environment.apiKey;
+
+    const url = `https://ws.audioscrobbler.com/2.0/?method=artist.getinfo&mbid=${this.mbid}&api_key=${apiKey}&format=json`;
+    const response = await fetch(url);
+    const data = await response.json();
+    console.log(this.mbid);
+    console.log(data);
+  }
+
+  async ngOnInit() {
+    this.mbid = this.route.snapshot.paramMap.get('artistId');
+    await this.getArtistInfo();
+  }
+}
